e60135e6875843dc5128d5acd99403fe99d269a3
Dave Steidel <dave.steidel@gmail.com>
2022-12-03 23:03:57 -0500

    go

--- src/day1/day1.go
+++ src/day1/day1.go
@@ -12,50 +12,40 @@ import (
 func part1() {
 	//f, err := os.Open("sample_input.txt")
 	f, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-    }
+	if err != nil { log.Fatal(err) }
 
     var cal int = 0
     var maxcal int = 0
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		if(len(scanner.Text()) > 0) {
+		if len(scanner.Text()) > 0 {
 			ii, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				log.Fatal(err)
-		    }
+			if err != nil { log.Fatal(err) }
 			cal += ii
 		} else {
-			if(cal > maxcal) {
-				maxcal = cal
-			}
+			if cal > maxcal { maxcal = cal }
 			cal = 0
 		}
 	}
 	f.Close()
 
-	fmt.Println("GO PART 1 maxcal =", maxcal)
+	fmt.Println("PART 1 maxcal =", maxcal)
 }
 
 func part2() {
 	//f, err := os.Open("sample_input.txt")
 	f, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-    }
+	if err != nil { log.Fatal(err) }
 
 	var cal int = 0
 	totals := make([]int, 0)
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		if(len(scanner.Text()) > 0) {
+		if len(scanner.Text()) > 0 {
 			ii, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				log.Fatal(err)
-		    }
+			if err != nil { log.Fatal(err) }
 			cal += ii
 		} else {
 			totals = append(totals, cal)
@@ -66,10 +56,10 @@ func part2() {
 
 	sort.Ints(totals)
 	total := totals[len(totals) - 3] + totals[len(totals) - 2] + totals[len(totals) - 1]
-	fmt.Println("GO PART 2 total =", total)
+	fmt.Println("PART 2 total =", total)
 }
 
 func main() {
-    part1()
+	part1()
 	part2()
 }
